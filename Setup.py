f8720ed956da10696e3be4aa4aa17b1e938d1ff4
G-Just <gjustas00@gmail.com>
2024-09-21 16:48:44 +0300

    Init

---
+++ Setup.py
@@ -0,0 +1,16 @@
+import pygetwindow
+import pyautogui
+import re
+
+activeWindows = pyautogui.getAllWindows()
+regex = re.compile("Alora.*")
+aloraClient = None
+for activeWindow in activeWindows:
+    if (re.search(regex, activeWindow.title)):
+        aloraClient = activeWindow
+
+if aloraClient is not None:
+    aloraClient.size = (1000, 700)
+    aloraClient.moveTo(0, 0)
+else:
+    print('Alora window is not found')
